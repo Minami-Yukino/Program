611f09493c91d125c090e9e7b2bc7d47a4c9a80c
Yukino Minami <46908989+Minami-Yukino@users.noreply.github.com>
2019-07-27 00:10:39 +0900

    Update and rename Untitled11.py to gammma.py

--- gammma.py
+++ gammma.py
@@ -1,18 +1,9 @@
-
-# coding: utf-8
-
-# In[48]:
-
-
 import scipy.special as scsp
-
-
-# In[49]:
-
 
 def lower_incomplete_gamma (a,b):
     #これでやっと第１種不完全ガンマ関数　γ(a,b）
     return scsp.gammainc(a,b)*scsp.gamma(a)
+
 def upper_incomplete_gamma(a,b):
     #これでやっと第２種不完全ガンマ関数　Γ(a,b）
     return scsp.gammaincc(a,b)*scsp.gamma(a)
@@ -20,22 +11,3 @@ def upper_incomplete_gamma(a,b):
 def gamma(a):
     #これでやっとガンマ関数　Γ(z)=z!
     return a*scsp.gamma(a)
-
-
-# In[50]:
-
-
-lower_incomplete_gamma(1,2)
-
-
-# In[51]:
-
-
-upper_incomplete_gamma(1,2)
-
-
-# In[64]:
-
-
-
-
